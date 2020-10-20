28baed3d50136e54e4cd2d08d9b6b66603474146
Chris Owens <guitarrockstarco@gmail.com>
2020-10-20 14:28:15 -0400

    Done with morning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+import VisibilityFilters from './components/VisibilityFilters';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Todo List - WTIH REDUX!!</h1>
+            <AddTodo />
+            <TodoList />
+            <VisibilityFilters />
+        </div>
+    );
 }
 
 export default App;
